a44e6b26fff4a345792321f84bc65a3943627acb
natwhite <git@nathaniel.sh>
2020-03-25 18:57:41 -0700

    Split mainMenu component into StartButton and Logo components.

--- src/sketch/scenes/options/options.scene.ts
+++ src/sketch/scenes/options/options.scene.ts
@@ -1,16 +1,17 @@
 import {EventEmitter} from 'events';
 import {Scene} from '../../core/Scene';
 import {BackgroundGraphics} from '../../global/components/backgroundGraphics.component';
-import {MainMenuComponent} from '../title/components/mainMenu.component';
+import {LogoComponent} from '../title/components/logo.component';
 import {RotatingGraphicComponent} from '../title/components/rotatingGraphic.component';
+import {StartButtonComponent} from '../title/components/startButton.component';
 
 export class OptionsScene extends Scene {
   public onStartClick: EventEmitter = new EventEmitter();
 
   constructor(s) {
     super(s);
-    const mainMenu = new MainMenuComponent(s, this.width, this.height);
-    mainMenu.onInteraction.on('click', () => {
+    const startButtonComponent = new StartButtonComponent(s, this.width, this.height);
+    startButtonComponent.onInteraction.on('click', () => {
       console.log(`Got click from MainMenu`);
       this.onStartClick.emit('transition');
     });
@@ -18,7 +19,8 @@ export class OptionsScene extends Scene {
       new BackgroundGraphics(s, this.width, this.height),
       // MenuController,
       new RotatingGraphicComponent(s, this.width, this.height),
-      mainMenu
+      new LogoComponent(s, this.width, this.height),
+      startButtonComponent
     ]);
   }
 }
